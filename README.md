2a4d1b4bc7c75afdf5acf2ec48b88e92ce9dfe80
alexrazo13 <alexrazo321@gmail.com>
2022-05-17 20:34:53 -0700

    commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # react-practice
 React Course
+practicing ES6
---
+++ script.js
@@ -0,0 +1,35 @@
+// console.log('es.6js');
+
+// let and const
+// let myVar = 'this';
+// const myChar = 'c';
+
+// arrow functions
+// const myFunc = () => {
+//     return console.log('this');
+// }
+
+// const myFunc = () => console.log('this);
+
+// myFunc();
+
+// template literals
+//let name = "Alex";
+
+// function changeName(newName) {
+//     return name = newName;
+// }
+
+const alexSentanceObject = {
+    name: 'Alex', //so use `` or ''?
+    location: 'Saturn',
+    timeZone: {
+        a: 'n',
+        b: 't'
+    }
+}
+
+const razoSentanceObject = {
+    name: 'Razo'
+    location: 'Moon'
+}
